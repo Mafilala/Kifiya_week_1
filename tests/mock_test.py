8d2760f7d703784662fa9360e562a0b707892e0f
Maruf Sherefa <marufsherefa9@gmail.com>
2024-08-29 11:19:44 +0300

    fixed test issue

--- tests/mock_test.py
+++ tests/mock_test.py
@@ -5,6 +5,7 @@ class MockTest(unittest):
         def setUp(self):
               pass
         
-
+        def mockT(self):
+              self.assertEqual('me', 'me')
 if __name__ == '__main__':
     unittest.main()
